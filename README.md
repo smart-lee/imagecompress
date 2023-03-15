472990001f8a9b27415e7820157543065c9092a5
lisl <lsl@hnfesco.com>
2023-03-15 16:25:58 +0800

    初始化

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # imagecompress
 大图片压缩，支持格式GIF、JPG、PNG、BMP，将图片大小控制在2M以内
+
+主要用到了PHP imagejpeg方法，
+图片大小小于2M，不压缩
+图片大小超过2M，按照阶梯压缩
---
+++ src/ImageCompress.php
@@ -0,0 +1,116 @@
+<?php
+namespace imagecompress;
+
+class ImageCompress
+{
+    /**
+     * @description 图片压缩
+     * 规则，2M以内不压缩，大于2M分阶梯压缩，直至压缩成2M内
+     * @param $filename 图片路径，相对路径且包含带后缀的文件名
+     * @return array|void 不需要接收返回值，因为压缩成功会替换源文件，且名称一样，还是$filename
+     * @author lisl 2023-03-15
+     */
+    public function compressImage($filename = '')
+    {
+        $result = [
+            'status' => -200,
+            'msg' => '',
+            'data' => [],
+        ];
+        try {
+            if (empty($filename) || !is_file($filename)) {
+                throw new \Exception('要压缩的图片不存在');
+            }
+            //filesize函数的结果会被缓存，使用此函数清除文件状态缓存
+            clearstatcache();
+            //filesize函数获得的是字节数，除以1024为KB数，再除以1024为MB数
+            $filesize = filesize($filename) / 1024 / 1024;
+            $noCompressMb = 2;
+            //小于2M不压缩
+            if ($filesize <= $noCompressMb) { //Mb数
+                throw new \Exception('要压缩的图片未到压缩条件');
+            }
+            $rand = range(1, 2, 0.1); //随机压缩成1-2M
+
+            //i的值影响压缩质量
+            if ($filesize > 50) {
+                $i = 80;
+            } elseif ($filesize > 30) {
+                $i = 90;
+            } elseif ($filesize > 10) {
+                $i = 95;
+            } else {
+                $i = 99;
+            }
+            //要压缩的图片路径
+            $path = pathinfo($filename);
+            $tempDir = $path['dirname']; //临时文件存放文件夹
+            $extension = $path['extension'];
+            if (!is_dir($tempDir)) { //文件夹不存在，创建
+                mkdir($tempDir);
+            }
+            $tmpFileList = []; //临时文件数组
+
+            do {
+                if ($i <= 0) { // 0:当到最差质量，跳出
+                    return;
+                }
+                //不影响响应时间的前提下，参数可以调节
+                if ($filesize > 50) {
+                    $step = 9;
+                } elseif ($filesize > 30) {
+                    $step = 8;
+                } elseif ($filesize > 10) {
+                    $step = 7;
+                } else {
+                    $step = 5;
+                }
+                $fileInfo = @getimagesize($filename);
+                $type = $fileInfo[2];
+                $imgResource = null;
+                //return ['jpg', 'jpeg', 'gif', 'bmp', 'png'];
+                switch ($type) {
+                    case 1:
+                        $imgResource = imagecreatefromgif($filename);
+                        $func = 'gif';
+                        break; // GIF
+                    case 2:
+                        $imgResource = imagecreatefromjpeg($filename);
+                        $func = 'jpg';
+                        break; // JPG
+                    case 3:
+                        $imgResource = imagecreatefrompng($filename);
+                        $func = 'png';
+                        break; // PNG
+                    case 6:
+                        $imgResource = imagecreatefrombmp($filename);
+                        $func = 'bmp';
+                        break; // BMP
+                }
+                $tmpFile = $tempDir . '/' . md5(uniqid(microtime(true), true)) . '.' . $extension; //临时文件
+//                $fun = "image" . $func;
+                $fun = "imagejpeg";
+                if (function_exists($fun)) {
+                    $fun($imgResource, $tmpFile, $i);//保存质量为$i的图片文件
+                }
+                imagedestroy($imgResource); //销毁图片资源
+                $tmpFileList[] = $tmpFile;
+                $i -= $step;
+                clearstatcache();
+                $filesize = filesize($tmpFile) / 1024 / 1024;
+            } while ($filesize > $noCompressMb);
+            rename($tmpFile, $filename); //压缩好的图片重命名为源图片
+            foreach ($tmpFileList as $tmpFile) { //删除临时文件
+                if (is_file($tmpFile)) {
+                    @unlink($tmpFile);
+                }
+            }
+            $result['status'] = 1;
+            $result['msg'] = 'success';
+            return $result;
+        } catch (\Exception $e) {
+            $result['msg'] = $e->getMessage();
+            return $result;
+        }
+    }
+}
